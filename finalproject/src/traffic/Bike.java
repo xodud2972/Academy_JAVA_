b95c9a151655d84054e31d9301bbe651668a2524
xodud2972 <xodud2972@naver.com>
2021-04-29 09:30:01 +0900

    0429commit

--- finalproject/src/traffic/Bike.java
+++ finalproject/src/traffic/Bike.java
@@ -1,10 +1,10 @@
 package traffic;
 
-public class Bike {
-	String name = "Harely Davison Fat Boy";
+public class Bike{
+	String name="Harley Davison Fat Boy";
 	int price=3000;
 
 	public void run(){
-		System.out.println(name+" is runing..");
+		System.out.println(name+" is running..");
 	}
 }
--- finalproject/src/use/UseBike.java
+++ finalproject/src/use/UseBike.java
@@ -1,9 +1,8 @@
 package use;
-import traffic.Bike; // traffic 이전 디렉토리 경로는 클래스패스(환경변수)에서 참조한다.
+import traffic.Bike; //traffic 이전 디렉토리 경로는 클래스패스에서 참조
 class UseBike{
-	public static void main(String[] argss){
-		Bike b = new Bike();
-
+	public static void main(String[] args){
+		Bike b=new Bike();
 		b.run();
 	}
 }
