22fbe6fa7074ba69522211c28169384c7df90a62
xodud2972 <xodud2972@naver.com>
2021-05-07 22:44:02 +0900

    0507commit

---
+++ app0507/src/app0507/ClassApp.java
@@ -0,0 +1,13 @@
+package app0507;
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI); // Math를 통해 pi 를 불러옴
+		System.out.println(Math.floor(1.6)); // 내림을 통해서 1.0을 출력 시킴
+		System.out.println(Math.ceil(1.6)); // 올림을 통해서 2.0을 출력 시킴
+		
+	}
+
+}
