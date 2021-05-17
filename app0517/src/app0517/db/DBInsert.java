2e0c7bd4a3e6973b305fd010ee54c9c4e229a7fa
xodud2972 <xodud2972@naver.com>
2021-05-17 17:43:44 +0900

    0517PM_commit

---
+++ app0517/src/app0517/db/DBInsert.java
@@ -0,0 +1,84 @@
+package app0517.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+//Mysql DB에 레코드 넣어보기!!
+//자바언어로 데이터베이스를 연동하는 기술을 가리켜 JDBC라 하며
+//java.sql 패키지에서 대부분의 api를 제공해준다..
+public class DBInsert {
+
+	public static void main(String[] args) {
+		//모든~~DB작업 수행은 절차가 동일하다
+		//0) 해당 DB에 알맞는 드라이버 클래스를 로드해야 함!!
+		//드라이버는 각 DB벤더사의 홈페이지 또는 저장소부터 다운받을 수 있는데, 
+		//이때 드라이버는 jar파일 형태로 되어 있다..
+		//jar(Java Archive) : 자바의 클래스를 압축시켜놓은 확장자를 jar 
+		
+		try {
+			// forName 메서드는 클래스를 static 영역으로 로드하는 메서드
+			Class.forName("com.mysql.jdbc.Driver");//드라이버 클래스 로드
+			System.out.println("드라이버 로드 성공");
+		} catch (ClassNotFoundException e1) {
+			System.out.println("해당 드라이버를 찾을 수 없습니다");
+			e1.printStackTrace();
+		}
+		
+		//1) DB접속 
+		String url="jdbc:mysql://localhost:3306/javase?characterEncoding=UTF-8";
+		String user="root";
+		String pass="1234";
+		Connection con = null;
+		PreparedStatement pstmt=null;//쿼리문 수행 객체
+		
+		try {
+			//아래의 코드에서 처럼, DriverManager에 의해 접속이 성공된 경우에만, 
+			//접속 정보를 가진 Connection 객체가 반환된다..따라서 접속에 실패한 경우는?
+			//Connection 객체가 반환되지 않으므로, null로 초기화된다..
+			con = DriverManager.getConnection(url, user, pass);
+			
+			if(con ==null) {
+				System.out.println("접속실패");
+			}else {
+				System.out.println("접속성공");
+				
+				String sql="insert into member(user_id, password, name)";
+				sql+=" values('batman','1111','뱃맨')";
+				
+				//위의 작성한 쿼리문을 실행해보자!!!
+				pstmt=con.prepareStatement(sql);//수행할 쿼리문 인수로 넣는다 
+				int result = pstmt.executeUpdate();//DML(insert , update ,delete) 쿼리실행
+				if(result==0) {
+					System.out.println("입력실패");
+				}else {
+					System.out.println("입력성공");
+				}				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		//2) 쿼리 수행 
+		//3) 접속끊기
+		
+
+	}
+
+}
---
+++ app0517/src/app0517/editor/MemoEditor.java
@@ -0,0 +1,152 @@
+package app0517.editor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+//메모장 기능중 스트림으로 처리할 수 있는 부분을 코드로 구현해본다!!
+public class MemoEditor extends JFrame{
+	JMenuBar bar;
+	JMenu m_file;
+	JMenuItem item_new; //새파일
+	JMenuItem item_open; //열기
+	JMenuItem item_save; //저장
+	JMenuItem item_saveas; //새이름으로 저장
+	JMenuItem item_exit; //끝내기
+	JTextArea area;
+	JScrollPane scroll;
+	JFileChooser chooser;//파일 탐색기
+	
+	public MemoEditor() {
+		//생성
+		bar = new JMenuBar();
+		m_file 		= new JMenu("파일");
+		item_new 	= new JMenuItem("새파일"); 
+		item_open 	= new JMenuItem("열기");
+		item_save 	= new JMenuItem("저장");
+		item_saveas = new JMenuItem("새이름으로 저장");
+		item_exit 	= new JMenuItem("끝내기");
+		area = new JTextArea();
+		scroll = new JScrollPane(area);
+		chooser = new JFileChooser("D:\\workspace\\korea202102_javaworkspace\\app0514\\src");
+		
+		//스타일 & 레이아웃 
+		
+		
+		//조립
+		m_file.add(item_new);
+		m_file.add(item_open);
+		m_file.add(item_save);
+		m_file.add(item_saveas);
+		m_file.add(item_exit);
+		
+		bar.add(m_file); //메뉴바에 파일메뉴 부착
+		
+		setJMenuBar(bar); //프레임에 메뉴 부착!!
+		add(scroll);
+		
+		//리스너연결
+		
+		//아래와 같은 이름없는 클래스를 가리켜 내부익명클래스라 한다..
+		//내부익명 클래스의 사용목적?  원래 클래스란 코드의 재사용성을 위한 거푸집이 목적이다..
+		//하지만, 클래스 중에는 재사용성이 별로 없는 1회성의 클래스도 있다..이런 경우 굳이 개발자가 .java파일을
+		//물리적으로 정의해가면서까지 개발할 필요가 있는가?? 따라서 sun에서는 클래스 안에 이름없는 클래스를 넣을수있도록
+		//내부익명 클래스를 지원해주며, 주로 이벤트 구현시 압도적으로 많이 사용된다...
+		//또한 내부익명 클래스를 사용하면, 객체간 주소를 전달해야하는 불편함도 해소할수 잇다..즉 내부익명클래스는 외부클래스
+		//의 멤버들을 자기껏 처럼 접근할 수 있다..
+		item_open.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				//파일열기 아이템을 누르면, JTextArea에 "안녕" 이라는 문자열을 출력해본다
+				openFile();				
+			}
+		});
+		
+		//새파일 처리 
+		item_new.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//새 파일은 area를 깨끗이 비워놓는 것임..
+				area.setText(""); //공백문자로 대체
+			}
+		});
+		
+		//새이름으로 저장 
+		item_saveas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/*	1) showSaveDialog() 호출하여 파일저장 탐색기 띄운다
+				 	2) 선택한 파일경로 얻는다 
+				 	3) 빈파일을 파일출력스트림으로 생성한다 
+				 	4) 생성된 파일출력스트림을 통해 area의 내용을 파일에 넣는다(출력)
+				 */	
+			}
+		});
+		
+		//보여주기
+		setSize(700,600);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //무언가 창을 닫을때, 자원을 반납하려면
+		// 이 메서드 호출대신 WindowListener를 구현하자!
+	}
+	
+	//파일 열어서, 편집기창에 출력하기 (불러오기)
+	public void openFile() {
+		int res=chooser.showOpenDialog(this);
+		
+		if(res==JFileChooser.APPROVE_OPTION) { 
+			//선택한 파일에 대한 입력스트림을 생성하여, 내용을 읽어와서 area에 출력!!
+			File file= chooser.getSelectedFile(); //선택한 파일 반환 받음
+			
+			//문자(영문,한글 모두: 즉 2byte를 이해함)를 읽을수 있는 스트림 생성
+			FileReader reader=null; //close하기 위해 밖으로 뺌
+			BufferedReader buffr=null; //문자스트림을 대상으로 버퍼를 지원하는 스트림
+			
+			try {
+				reader = new FileReader(file);//파일을 대상으로 빨대 꽂음
+				buffr = new BufferedReader(reader); //버퍼처리된 스트림으로 빨대업그레이드
+				
+				String data=null; //읽혀진 문자(정확히는 문자의 코드)를 담게될 정수
+				
+				while(true) {
+					data = buffr.readLine(); //한 문자열 즉 한 줄(케릭터들을 모아서) 읽기
+					if(data==null)break;
+					area.append(data+"\n");
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {				
+				if(buffr!=null) {
+					try {
+						buffr.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				if(reader!=null) {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		new MemoEditor();
+	}
+}
+
