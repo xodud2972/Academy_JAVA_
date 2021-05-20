ea56f2da02df614432363c7cdcf0db23e814f1e2
xodud2972 <xodud2972@naver.com>
2021-05-20 17:51:37 +0900

    0520commit_AM

---
+++ app0520/src/db/table/TableBasic.java
@@ -0,0 +1,133 @@
+package db.table;
+
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+//DBMS 등의 표형식(2차원구조)의 데이터를 표현하는 최적화된 JTable을 이용해본다
+public class TableBasic extends JFrame{
+	//JTable의 생성자 중, 레코드는 이차원배열로 지원하고, 컬럼의 제목은 일차원배열로 지원하는 3번째 생성자를 이용해보자!!
+	String[] columns= {"member_id","user_id", "password","name","regdate"};
+	String[][] rows= {}; //생성되지도 않음
+	JTable table;
+	JScrollPane scroll;
+	
+	//mysql 접속 문자열에 사용될 접속정보
+	String url="jdbc:mysql://localhost:3306/javase?characterEncoding=UTF-8";
+	String user="root";
+	String password="1234";
+	Connection con; //접속 후 그 정보를 가진 객체
+	
+	public TableBasic() {
+		connect();
+		
+		table = new JTable(rows, columns); 
+		scroll = new JScrollPane(table);
+		
+		add(scroll);
+		
+		setVisible(true);
+		setSize(500, 250);
+		//setDefaultCloseOperation(EXIT_ON_CLOSE);//자원을 해제시킬 작업이 없기 때문에 이용가능...
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if(con !=null) {
+					try {
+						con.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}	
+			}
+		});
+		
+	}
+	//데이터베이스 가져오기
+	public void connect(){
+		/*
+		 * 1) 드라이버 로드 
+		 * 2) 접속 
+		 * 3) 쿼리수행
+		 * 4) 자원해제
+		 * */
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			JOptionPane.showMessageDialog(this, "MySQL 드라이버 로드 성공");
+			
+			con=DriverManager.getConnection(url, user, password);
+			
+			if(con!=null) {
+				JOptionPane.showMessageDialog(this, "접속 성공");
+				select();
+			}else {
+				JOptionPane.showMessageDialog(this, "접속 실패");
+			}
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(this, "드라이버 로드 실패\n드라이버를 찾을 수 없습니다");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//select문을 수행하는 메서드 정의
+	public void select() {
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="select * from member";
+		
+		try {
+			pstmt=con.prepareStatement(sql, 
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY); //쿼리수행 객체 생성
+			//위의 옵션을 줄 경우 스크롤 가능하면서 읽기전용의 ResultSet 을 생성할 수 있다..
+			rs=pstmt.executeQuery(); //쿼리실행 및 레코드 반환!!
+			
+			//rs의 데이터를 이차원배열로 변환하여, JTable에게 적용시켜보자!!
+			//rs에는 총 레코드수를 반환해주는 메서드가 별도로 지원되지 않는다..따라서 개발자가 다음의 절차를 거쳐 
+			//총 레코드수를 구할 수 있다 
+			//[1]커서를 레코드의 제일 마지막으로 이동 !! last() 
+			//[2]마지막으로 이동된 커서의 row 넘버를 구한다. 여기서 넘버란 몇번째 레코드인지에 대한 순번이다..
+			//만일 rowNum이 3이라면 3번째 데이터라는 의미다!
+			rs.last();
+			int num = rs.getRow(); //현재의 레코드 순번을 반환받아보자
+			System.out.println("저의 현재 위치는 "+ num);
+			
+			String[][] data = new String[ num ][columns.length];
+			
+			rs.beforeFirst();
+			
+			int index = 0;
+			while(rs.next()) {
+				// rs의 레코드에 접근하며, empty 상태에 있는 2차원 배열로 데이터를 옮겨보자.
+				data[index][0] = Integer.toString(rs.getInt("member_id")); // int 형을 String 객체형으로 변환 , 이떄 사용되는 객체가 Wrapper
+				data[index][1] = rs.getString("user_id");
+				data[index][2] = rs.getString("password");
+				data[index][3] = rs.getString("name");
+				data[index][4] = rs.getString("regdate");
+				index++;
+			}
+			table.setValueAt(data[0][0], 0, 0);
+			table.updateUI(); // 갱신된 데이터를 이용하여 JTable
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new TableBasic();
+	}
+
+}
---
+++ app0520/src/db/table/model/UseModelApp.java
@@ -0,0 +1,272 @@
+package db.table.model;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
+
+/*
+ * JTable이 사용할 데이터를, TableModel을 이용해서 좀더 유지보수성에 있어 개선을 해보자
+ * TableModel 이란? JTable 과 같은 디자인영역에서 실제 데이터를 직접 접근하지 말고, 중간에 
+ * 계층을 두고, 데이터의 조회,입력, 수정등을 이 중간 계층을 통해 작업을 진행하면 추후 Jtable이 변경될때 
+ * 데이터를 처리하려는 로직은 영향을 받지 않게 되므로, 어플리케이션의 유지보수성을 높일 수 있다.. 
+ * TableModel 은 JTable 사용하려는 데이터를 간접적으로 사용할수 있게 도와주는 컨트롤러를 의미 
+ * */
+public class UseModelApp extends JFrame{
+	JTable table;
+	JScrollPane scroll;
+	JTextArea area; //선택한 회원의 상세정보 출력할 영역
+
+	JPanel p_west;
+	JTextField t_user_id;
+	JPasswordField t_password;
+	JTextField t_name;
+	JButton bt_regist, bt_del;
+	
+	String[] columnName= {"member_id","user_id","password","name","regdate"};
+	String[][] rows= {};
+
+	String url="jdbc:mysql://localhost:3306/javase?characterEncoding=UTF-8";
+	String user="root";
+	String password="1234";
+	Connection con;
+	UseModelApp uma = this;
+	
+	public UseModelApp() {
+		table = new JTable(new AbstractTableModel() {
+			
+			//총 레코드수를 반환하는 메서드 그리고 이 메서드는 JTable이 호출하는 거다!!
+			public int getRowCount() {//층수
+				return rows.length;
+			}
+			//총 컬럼수를 반환하는 메서드, 그리고 이 메서드 또한 JTable이 호출하는 거다!!
+			public int getColumnCount() {
+				return columnName.length;
+			}
+			
+			public String getColumnName(int col) { //col 변수에 호출시마다 1씩 증가시키며 호출 0,1,2,3,4
+				return columnName[col];
+			}
+			
+			//row, col 좌표에 어떤 데이터가 있는 반환 하는 메서드, 이 또한 JTable이 호출하는 거다!!
+			//즉 TableModel의 모든 메서드가 다 JTable을 위한 메서드이다!!
+			public Object getValueAt(int row, int col) {
+				System.out.println("getValueAt("+row+", "+col+")");
+				return rows[row][col];
+			}
+		}); // 테이블모델 객체 이용가능
+		
+		scroll = new JScrollPane(table);
+		area = new JTextArea();
+		
+		//서쪽의 등록폼 생성 
+		p_west = new JPanel();
+		t_user_id = new JTextField(15);
+		t_password = new JPasswordField(15);
+		t_name = new JTextField(15);
+		bt_regist = new JButton("등록");
+		bt_del = new JButton("삭제");
+		
+		
+		//스타일
+		scroll.setPreferredSize(new Dimension(600,400));
+		area.setPreferredSize(new Dimension(600, 150));
+		p_west.setPreferredSize(new Dimension(200,400));
+		
+		//조립 
+		p_west.add(t_user_id);
+		p_west.add(t_password);
+		p_west.add(t_name);
+		p_west.add(bt_regist);
+		p_west.add(bt_del);
+		
+		add(scroll);
+		add(area, BorderLayout.SOUTH);
+		add(p_west, BorderLayout.WEST);
+		
+		bt_regist.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				regist();
+				
+			}
+		});
+		
+		bt_del.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showConfirmDialog(uma ,"정말로 삭제 하시겠습니까?");
+				
+			}
+		});
+		
+		
+		//테이블에 클릭 이벤트 구현 
+		table.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				//area.append("해당 레코드 클릭햇어?\n");
+				int row = table.getSelectedRow();
+				int col = table.getSelectedColumn();
+				String value = (String)table.getValueAt(row, col);
+				//area.setText("당신이 선택한 위치는 "+row+", "+col+" 그리고 그 좌표에 들어있는 데이터 값은 "+value);
+				area.setText("");//출력전에 초기화
+				area.append("member_id : "+table.getValueAt(row, 0)+"\n");
+				area.append("아이디 : "+table.getValueAt(row, 1)+"\n");
+				area.append("비밀번호 : "+table.getValueAt(row, 2)+"\n");
+				area.append("이름 : "+table.getValueAt(row, 3)+"\n");
+				area.append("가입일 : "+table.getValueAt(row, 4)+"\n");
+				
+			
+			}
+		});
+		
+		//윈도우와 리스너 연결 
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if(con!=null) {
+					try {
+						con.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}	
+				System.exit(0); //kill process !!!
+			}
+		});
+		
+		setSize(800,400);
+		setVisible(true);
+		
+		connect();
+	}
+	
+	public void connect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");//드라이버 로드
+			con = DriverManager.getConnection(url, user, password);//접속
+			if(con==null) {
+				JOptionPane.showMessageDialog(this, "MySQL 접속 실패");
+			}else {
+				getList();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getList() {
+		PreparedStatement pstmt=null;
+		ResultSet rs = null;
+		String sql="select * from member";
+		
+		try {
+			pstmt=con.prepareStatement(sql
+					, ResultSet.TYPE_SCROLL_INSENSITIVE
+					, ResultSet.CONCUR_READ_ONLY);
+			
+			rs=pstmt.executeQuery();//레코드 담기!!
+			rs.last(); //커서를 마지막 레코드로 이동
+			int total = rs.getRow();
+			
+			String[][] data = new String[total][columnName.length]; //empty 이차원 배열 생성
+			//주의할 점)  스크롤가능한 옵션을 줘야 하고, 커서의 위치에 주의하자!!
+			//rs의 정보를 이차원배열로 옮겨보기!!
+			rs.beforeFirst(); //커서를 다시 처음 위치로 
+			
+			int index=0;
+			while(rs.next()) {
+				data[index][0]=Integer.toString(rs.getInt("member_id"));
+				data[index][1]=rs.getString("user_id");
+				data[index][2]=rs.getString("password");
+				data[index][3]=rs.getString("name");
+				data[index][4]=rs.getString("regdate");
+				index++;
+			}
+			
+			rows=data;//rs를 이차원배열로 바꾼다!!	
+			table.updateUI();//디자인 갱신 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	//등록
+	public void regist() {
+		PreparedStatement pstmt = null;
+		
+		String sql = "insert into member(user_id, password, name) values(?,?,?)";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, t_user_id.getText());
+			pstmt.setString(2, new String(t_password.getPassword()));
+			pstmt.setString(3, t_name.getText());
+			int result = pstmt.executeUpdate(); //이 DML에 실행에 의해 영향을 받은 record 수를 반환함.
+			// 따라서 insert의 경우 제대로 수행되었다면 1이 반환되어야 한다.
+			if(result == 1) {
+				JOptionPane.showMessageDialog(this, "등록 성공");
+			}else {
+				JOptionPane.showMessageDialog(this, "등록 실패");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if (pstmt!=null) {
+				try {
+					pstmt.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	//삭제
+	public void del() {
+		
+	}
+	public static void main(String[] args) {
+		new UseModelApp();
+
+	}
+
+	
+
+}
