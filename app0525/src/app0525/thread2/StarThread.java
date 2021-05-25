99c06e01d061453df8d904dfae19e11b74f77783
xodud2972 <xodud2972@naver.com>
2021-05-25 17:43:32 +0900

    0525commit_PM

---
+++ app0525/src/app0525/thread2/StarThread.java
@@ -0,0 +1,12 @@
+package app0525.thread2;
+
+//무한루프로 별을 출력하는 실행단위
+public class StarThread extends Thread{	
+	public void run() {
+		while(true) {
+			System.out.println("☆");
+			System.out.println("☆");
+			System.out.println("☆");
+		}
+	}
+}
