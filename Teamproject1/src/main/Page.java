b4484b3b0f4ad774f00b54593a1728561707e38f
xodud2972 <xodud2972@naver.com>
2021-06-13 17:42:35 +0900

    0613

---
+++ Teamproject1/src/main/Page.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class Page extends JPanel{
+	private AppMain appMain;
+
+	
+	public AppMain getAppMain() {
+		return appMain;
+	}
+
+	
+	public void setAppMain(AppMain appMain) {
+		this.appMain = appMain;
+		setPreferredSize(new Dimension(1160, 800));
+		setVisible(false);
+	}
+	
+	public Page(AppMain appMain) {
+		this.appMain=appMain;
+		setPreferredSize(new Dimension(1200, 700));
+		setVisible(false);
+	
+}
+	}
